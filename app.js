e76b8bad5e1c0c8e4e7826a63d2480775a22836d
Jhonny Izidoro Menarim <jhonny.menarim@ecompleto.com.br>
2020-03-04 12:07:24 -0300

    CKEDITOR

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use('/', express.static(`${__dirname}/public`))
 app.use('/', express.static(`${__dirname}/public/global`))
 app.use(assets('/', `${__dirname}/public`))
-app.use(fileUpload({ limits: { fileSize: 5 * 1024 * 1024 }, }))
+app.use(fileUpload({}))
 app.use(session({
 	saveUninitialized: true,
 	secret: 'apasNodeJS',
